5d2aca35f555987c5260d2f6e2ce323feff761f4
Syed Ahsan Ahmed <snahmed1998@gmail.com>
2018-06-12 11:39:18 +0500

    header, message added

--- router.js
+++ router.js
@@ -1,25 +1,39 @@
 var commonHeader = { 'Content-Type': 'html' };
 const render = require('./render');
+var qs = require('querystring');
 
 function home(req, res)
 {
 	res.writeHead(200, commonHeader);
 	//home page
-	if (req.url === "/") 
+	if (req.url == "/Clipboard")
 	{
-		render.view("home", {homepage: 1}, res);
-		res.write("This is our home page");
+		res.write("you are viewing clipboard");
 		res.end();
 	}
-	else{
-		//some response
-		res.write("This is our the requested page");
-		req.on("data", function (postBody) {
-        var query = querystring.parse(postBody.toString());
-        res.writeHead(303, { "location": "/" + query.message });
-        res.end();
-      	})
-	}
+	if (req.method == 'POST') {
+        var body = '';
+
+        req.on('data', function (data) {
+            body += data;
+            if (body.length > 1e6)
+                req.connection.destroy();
+        });
+
+        req.on('end', function () {
+            var post = qs.parse(body);
+            var name = post['message']
+            render.view("header",{}, res);
+            render.view("success_message",{}, res);
+            render.view("home",{}, res);
+            res.end();
+        });
+    }
+    else{
+    	render.view("header",{}, res);
+    	render.view("home",{}, res);
+		res.end();
+    }
 }
 
 module.exports.home = home;
